0fb119e8bff8e2e87f023aab0ad2f08598a37e91
Shewon Gunarathne <shewongunarathne@gmail.com>
2026-04-16 16:31:49 +0530

    vercel.json file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const apiBase = env.VITE_API_BASE_URL || 'http://20.247.169.212:3000/api'
-  const proxyTarget = apiBase.replace(/\/api\/?$/i, '')
+  const proxyTarget = /^https?:\/\//i.test(apiBase)
+    ? apiBase.replace(/\/api\/?$/i, '')
+    : 'http://20.247.169.212:3000'
 
   return {
     plugins: [
